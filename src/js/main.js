5268499d19184bb72fc4d2156b0222c7434071a9
Mercury Rory <Mercury Rory>
2017-03-22 08:32:30 +0300

    Init commit.

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+/**********
+
+  THIRD PARTY
+
+***********/
+// Included CDN jQuery
+// ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/jquery.easing/js/jquery.easing.min.js
+//= ../../bower_components/jquery-color/jquery.color.js
+//= ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js
+// You need slicker?
+// ../../bower_components/slick-carousel/slick/slick.min.js
+/**********
+
+  CUSTOM
+
+***********/
+//= app.js
