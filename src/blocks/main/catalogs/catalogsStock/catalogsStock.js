5ea1b4f0fcec935988043d56ecf284a01c0ea2af
Wishez <wishez@list.ru>
2017-03-23 22:35:09 +0300

    Fixed the responsive about and ctalogs sections. Making footer.

--- src/blocks/main/catalogs/catalogsStock/catalogsStock.js
+++ src/blocks/main/catalogs/catalogsStock/catalogsStock.js
@@ -2,5 +2,16 @@ $('.catalogsStock').slick({
   lazyLoad: 'ondemand',
   slidesToShow: 4,
   slidesToScroll: 1,
-  arrows: true
+  arrows: true,
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+       slidesToShow: 2,
+    }  
+  }, {
+    breakpoint: 991,
+    settings: {
+       slidesToShow: 3,
+    }
+  }]
 });
