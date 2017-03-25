3cd4d4cd15f6bb830291b2ad1cb2f8e2bc199a6f
Wishez <wishez@list.ru>
2017-03-25 12:56:01 +0300

    Validate form.

--- src/js/additions/app.js
+++ src/js/additions/app.js
@@ -1,6 +1,11 @@
 // Add your moduls if you need.
 (function(global) {
   //= ../../blocks/custom/custom.js
+  //= ../../snippets/registr/main/main.js
+  //= ../../snippets/videos/main/main.js
+  //= ../../snippets/shares/main/main.js
+  //= ../../snippets/contacts/main/main.js
+  //= ../../snippets/opportunities/main/main.js
  global.$cmt = cmt;
 })(window);
 
@@ -10,4 +15,9 @@ $(function() {
   //= ../../blocks/custom/custom-controller.js
   //= ../../blocks/header/navigation/navigation.js
   //= ../../blocks/main/main-controller.js
+  //= ../../snippets/registr/main/main-controller.js
+  //= ../../snippets/videos/main/main-controller.js
+  //= ../../snippets/shares/main/main-controller.js
+  //= ../../snippets/contacts/main/main-controller.js
+  //= ../../snippets/opportunities/main/main-controller.js
 });//end ready
