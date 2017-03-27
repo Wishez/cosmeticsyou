efafb5ac4be45c7ab3a5ff8e5f4cba22ce811046
Wishez <wishez@list.ru>
2017-03-27 15:45:22 +0300

    Added loading.

--- src/blocks/custom/custom-controller.js
+++ src/blocks/custom/custom-controller.js
@@ -1,8 +1,16 @@
 // jshint esversion: 6
 $(document).on('click', '.officialSite__refer, .address__item, .submitForm__agreement, .submitForm__conditions', (e) =>  {
-  let url = $(this).attr('href');
+  let url = $(this).prop('href');
   
   window.open(url);
   
   e.preventDefault();
+}); // end click
+
+$cmt.showLoading('#mainContent');
+
+$(document).on('click', '.navItem', function() {
+  $cmt.switchMenu(this);
+  
+  return false;
 }); // end click
--- src/blocks/custom/custom.js
+++ src/blocks/custom/custom.js
@@ -1,1 +1,19 @@
-var cmt = {};
+// jshint esversion: 6
+let cmt = {},
+    select = {
+      $main: $('#mainContent')
+    };
+
+
+cmt.showLoading = elem => {
+  let img = '<img src="../img/loader/ajax-loader.svg" alt="" style="margin: 50px auto; display: block;" />';
+  
+  $(elem).html(img);
+};
+
+cmt.switchMenu = elem => {
+  $('.navItem').removeClass('active');
+  
+  $(elem).addClass('active');
+};
+
