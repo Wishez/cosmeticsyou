b91372ac7be6ab0789bef7c8d18faf95ed9e2bab
Filipp Zhuravlev <shiningfinger@list.ru>
2018-01-13 19:27:08 +0300

    Upadte task manager. I have removed rigger and add webpack for js task. Also, I shared tasks by files for their convinient editing.

---
+++ cosmeticsyou/theme/gulp/tasks/copy.js
@@ -0,0 +1,50 @@
+var gulp   = require('gulp');
+var config = require('../config.js');
+var image  = require('gulp-image');
+
+gulp.task('copy:fonts', function() {
+    return gulp
+        .src(config.src.fonts + '/*.{svg,ttf,eot,woff,woff2}')
+        .pipe(gulp.dest(config.dest.fonts));
+});
+
+gulp.task('copy:lib', function() {
+    return gulp
+        .src(config.src.lib + '/**/*.*')
+        .pipe(gulp.dest(config.dest.lib));
+});
+
+gulp.task('copy:rootfiles', function() {
+    return gulp
+        .src(config.src.root + '/*.*')
+        .pipe(gulp.dest(config.dest.root));
+});
+
+gulp.task('copy:img', function() {
+    return gulp
+        .src([
+            config.src.img + '/**/*.{jpg,png,jpeg,svg,gif}',
+            '!' + config.src.img + '/svgo/**/*.*'
+        ])
+        .pipe(gulp.dest(config.dest.img));
+});
+
+gulp.task('copy:img:min', function() {
+    return gulp
+        .src([
+            config.src.img + '/**/*.{jpg,png,jpeg,svg,gif}',
+            '!' + config.src.img + '/svgo/**/*.*'
+        ])
+        .pipe(image())
+        .pipe(gulp.dest(config.dest.img));
+});
+const imageTask = process.env.NODE_ENV == 'build' ? 'copy:img:min' : 'copy:img';
+gulp.task('copy', [
+    imageTask,
+    // 'copy:rootfiles',
+    'copy:lib',
+    'copy:fonts'
+]);
+gulp.task('copy:watch', function() {
+    gulp.watch(config.src.img+'/*', ['copy']);
+});
---
+++ cosmeticsyou/theme/gulp/tasks/pug.js
@@ -0,0 +1,28 @@
+var gulp    = require('gulp');
+var pug     = require('gulp-pug');
+var config  = require('../config.js');
+var plumber = require('gulp-plumber');
+
+function emmitTemplates(pathFrom, pathTo) {
+    return gulp.src(pathFrom)
+        .pipe(plumber())
+        .pipe(pug({
+            pretty: false
+        }))
+        .pipe(gulp.dest(pathTo));
+}
+
+gulp.task('html:pages', () => {
+    return emmitTemplates(config.src.pages, config.dest.pages);
+});
+gulp.task('html:accounts', () => {
+    return emmitTemplates(config.src.accounts, config.dest.accounts);
+});
+gulp.task('html:shares', () => {
+    return emmitTemplates(config.src.shares, config.dest.shares);
+});
+gulp.task('html', ['html:pages', 'html:accounts', 'html:shares']);
+
+gulp.task('html:watch', function() {
+    gulp.watch(config.src.html + '/**/*.{html}', ['html']);
+});
