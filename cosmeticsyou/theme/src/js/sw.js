84f4f078b9019dae58bfee142e2ba5c416d87d7f
Filipp Zhuravlev <shiningfinger@list.ru>
2018-01-19 10:57:49 +0300

    Change pathes of caching static files.

--- cosmeticsyou/theme/src/js/sw.js
+++ cosmeticsyou/theme/src/js/sw.js
@@ -3,13 +3,52 @@
 // 3. Confirm whether all the requored assets are cached or not
 
 var CACHE_NAME = 'cosmeticsyou-cache-v1';
-var staticCssPath = '/static/cosmeticsyou/js/';
-var staticJsPath = '/static/cosmeticsyou/'; 
+var staticCssPath = '/static/cosmeticsyou/css/';
+var staticJsPath = '/static/cosmeticsyou/js/';
+var staticFontPath = '/static/cosmeticsyou/fonts/'; 
 
 var urlsToCache = [
-  staticCssPath + '/main.css',
-  staticJsPath + '/main.js',
-  staticJsPath + '/modernizr.js'
+  staticCssPath + 'main.css',
+  staticCssPath + 'fonts.css',
+
+  staticJsPath + 'main.js',
+  staticJsPath + 'registration.js',
+
+  staticFontPath + 'Politica/Politica.eot',
+  staticFontPath + 'Politica/Politica.ttf',
+  staticFontPath + 'Politica/Politica.woff',
+  staticFontPath + 'Politica/Politica.woff2',
+  staticFontPath + 'Politica/Politica.svg',
+
+  staticFontPath + 'Politica/Politica-Bold.eot',
+  staticFontPath + 'Politica/Politica-Bold.ttf',
+  staticFontPath + 'Politica/Politica-Bold.woff',
+  staticFontPath + 'Politica/Politica-Bold.woff2',
+  staticFontPath + 'Politica/Politica-Bold.svg',
+
+  staticFontPath + 'Politica/Politica-ExtraBold.eot',
+  staticFontPath + 'Politica/Politica-ExtraBold.svg',
+  staticFontPath + 'Politica/Politica-ExtraBold.ttf',
+  staticFontPath + 'Politica/Politica-ExtraBold.woff',
+  staticFontPath + 'Politica/Politica-ExtraBold.woff2',
+
+  staticFontPath + 'Roboto/Roboto-Regular.eot',
+  staticFontPath + 'Roboto/Roboto-Regular.svg',
+  staticFontPath + 'Roboto/Roboto-Regular.ttf',
+  staticFontPath + 'Roboto/Roboto-Regular.woff',
+  staticFontPath + 'Roboto/Roboto-Regular.woff2',
+
+  staticFontPath + 'Roboto/Roboto-Bold.eot',
+  staticFontPath + 'Roboto/Roboto-Bold.svg',
+  staticFontPath + 'Roboto/Roboto-Bold.ttf',
+  staticFontPath + 'Roboto/Roboto-Bold.woff',
+  staticFontPath + 'Roboto/Roboto-Bold.woff2',
+
+  staticFontPath + 'Roboto/Roboto-Italic.eot',
+  staticFontPath + 'Roboto/Roboto-Italic.svg',
+  staticFontPath + 'Roboto/Roboto-Italic.ttf',
+  staticFontPath + 'Roboto/Roboto-Italic.woff',
+  staticFontPath + 'Roboto/Roboto-Italic.woff2'
 ];
 
 self.addEventListener('install', function(e) {
