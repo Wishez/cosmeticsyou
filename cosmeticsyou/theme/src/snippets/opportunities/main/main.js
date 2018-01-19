08e113ba04d06d8f5c1441639d610deca932fffc
Filipp Zhuravlev <shiningfinger@list.ru>
2018-01-19 10:57:20 +0300

    Delete old files.

--- cosmeticsyou/theme/src/snippets/opportunities/main/main.js
+++
@@ -1,6 +0,0 @@
-// jshint esversion: 6
-// cmt.showBusinessPage = () => {
-//   r.getBusinessPage().done(businessPage => {
-//     select.$main.html(businessPage);
-//   });
-// };
--- cosmeticsyou/theme/src/snippets/videos/main/main.js
+++
@@ -1,5 +0,0 @@
-cmt.showVideosPage = () => {
-  r.getVideosPage().done(videosPage => {
-    select.$main.html(videosPage);
-  });
-};
